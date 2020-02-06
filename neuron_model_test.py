bfb102ba2119118fb05c436cebd86bed6512b726
HanjiaJiang <bagheera1984@gmail.com>
2020-02-06 16:57:35 +0100

    neuron_model_test.py: add pv and som cells membrane parameters

--- neuron_model_test.py
+++ neuron_model_test.py
@@ -4,6 +4,57 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['font.size'] = 20.0
 
+# to be done
+# exc = {
+#     # Reset membrane potential of the neurons (in mV).
+#     'E_L': -63.3,
+#     # Threshold potential of the neurons (in mV).
+#     'V_th': -41.0,
+#     # Membrane potential after a spike (in mV).
+#     'V_reset': -67.0,  # -65.0,
+#     # Membrane capacitance (in pF).
+#     'C_m': 322.0,
+#     # Membrane time constant (in ms).
+#     'tau_m': 13.0,
+#     # Time constant of postsynaptic excitatory currents (in ms).
+#     'tau_syn_ex': 1.74,  # 1.9,  # Allen mouse #1.74, #1.0, # 0.5,
+#     # Time constant of postsynaptic inhibitory currents (in ms).
+#     'tau_syn_in': 4.6,  # 2.9,  # Allen mouse #4.6, #2.0, # 0.5,
+# }
+
+pv = {
+    # Reset membrane potential of the neurons (in mV).
+    'E_L': -66.8,
+    # Threshold potential of the neurons (in mV).
+    'V_th': -40.5,
+    # Membrane potential after a spike (in mV).
+    'V_reset': -67.0,  # -65.0,
+    # Membrane capacitance (in pF).
+    'C_m': 86.2,
+    # Membrane time constant (in ms).
+    'tau_m': 3.6,
+    # Time constant of postsynaptic excitatory currents (in ms).
+    'tau_syn_ex': 1.74,  # 1.9,  # Allen mouse #1.74, #1.0, # 0.5,
+    # Time constant of postsynaptic inhibitory currents (in ms).
+    'tau_syn_in': 4.6,  # 2.9,  # Allen mouse #4.6, #2.0, # 0.5,
+}
+
+som = {
+    # Reset membrane potential of the neurons (in mV).
+    'E_L': -61.6,
+    # Threshold potential of the neurons (in mV).
+    'V_th': -40.3,
+    # Membrane potential after a spike (in mV).
+    'V_reset': -67.0,  # -65.0,
+    # Membrane capacitance (in pF).
+    'C_m': 134.0,
+    # Membrane time constant (in ms).
+    'tau_m': 11.8,
+    # Time constant of postsynaptic excitatory currents (in ms).
+    'tau_syn_ex': 1.74,  # 1.9,  # Allen mouse #1.74, #1.0, # 0.5,
+    # Time constant of postsynaptic inhibitory currents (in ms).
+    'tau_syn_in': 4.6,  # 2.9,  # Allen mouse #4.6, #2.0, # 0.5,
+}
 
 n_model = 'iaf_psc_exp'
 defaults = nest.GetDefaults(n_model)
@@ -11,8 +62,8 @@ syn_weight = 20.0
 sim_time = 500.0
 
 # spike generator
-isi = 30.0
-spike_times = np.arange(isi, sim_time, isi)
+isi = 20.0
+spike_times = np.arange(0.1, sim_time, isi)
 spk = nest.Create('spike_generator')
 nest.SetStatus(spk, {'spike_times': spike_times})
 
@@ -22,10 +73,10 @@ syn_dict_n = {
 
 syn_dict_d = {
     'model': 'tsodyks_synapse',
-    'U': 0.5,
-    'tau_fac': 100.0,
+    'U': 0.75,
+    'tau_fac': 0.0,
     'tau_psc': defaults['tau_syn_ex'],
-    'tau_rec': 200.0, #800.0,
+    'tau_rec': 100.0, #800.0,
     'weight': syn_weight
 }
 
@@ -38,8 +89,10 @@ syn_dict_f = {
     'weight': syn_weight
 }
 
+
 # depressed
 nd1 = nest.Create(n_model)
+nest.SetStatus(nd1, pv)
 nd2 = nest.Create('parrot_neuron')
 # nest.SetStatus(nd2, {'I_e': 450.0})
 
@@ -53,6 +106,7 @@ nest.Connect(md, nd1)
 
 # facilitated
 nf1 = nest.Create(n_model)
+nest.SetStatus(nf1, som)
 nf2 = nest.Create('parrot_neuron')
 # nest.SetStatus(nf2, {'I_e': 450.0})
 
@@ -73,7 +127,7 @@ ts = dmm["events"]["times"]
 
 plt.plot(ts, Vms, 'g')
 plt.tight_layout()
-plt.ylim((-70.0, -69.8))
+plt.ylim((pv['E_L'], pv['E_L'] + 0.2))
 plt.xlabel('t (ms)')
 plt.ylabel('V')
 plt.title('depressed (E to PV)')
@@ -86,7 +140,7 @@ Vms = dmm["events"]["V_m"]
 ts = dmm["events"]["times"]
 plt.plot(ts, Vms, 'b')
 plt.tight_layout()
-plt.ylim((-70.0, -69.8))
+plt.ylim((som['E_L'], som['E_L'] + 0.2))
 plt.xlabel('t (ms)')
 plt.ylabel('V')
 plt.title('facilitated (E to SOM)')
